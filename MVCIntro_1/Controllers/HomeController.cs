b88a9b6fa0a994fb60f16b4beefd83029e338ecd
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-08 14:25:06 +0300

    Add project files.

---
+++ MVCIntro_1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntro_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //MVC'de Action'lar cok önemlidir...Cünkü BackEnd işlemlerinizi, HTTP'ye göre yapmak istediginiz tüm işlemlerinizi Controller icerisindeki bu Action'lar yapar...
+
+        //Eger MVC'de bir Action'a HTTP request tipi vermezseniz, Action otomatik olarak HTTPGet yönteminde calısır...
+
+        //MVC'de url'e yazdınız controller yoksa veya controller varsa ama icindeki action yoksa aynı hatayı alırsınız(Resource can not be found)
+
+        //MVC'de projenizin sayfayı size getirebilmesi icin sadece Controller ve Action yetmez...Action'in döndürecegi bir View olması da gerekir..Yani bir sayfa
+        public HomeController()
+        {
+            
+        }
+
+
+
+        public ActionResult Index()
+        {
+            //Eger bir View() metodu bir argüman almadan cagrılmıssa icinde bulundugu Action ile aynı isme sahip olan bir View'i döndürmeye calısır...
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
+
+
---
+++ MVCIntro_1/Controllers/SimulationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntro_1.Controllers
+{
+    public class SimulationController : Controller
+    {
+        // GET: Simulation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
+//ViewResult
